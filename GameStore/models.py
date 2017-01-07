38c70c19f1bc71cb0d56679f8824b6d73831351c
Nathan Marcalle <NathanMarcalle@Nathan-MBP.home>
2017-01-07 22:24:56 +0200

    Added ability to rate games

--- GameStore/models.py
+++ GameStore/models.py
@@ -30,10 +30,13 @@ class Game(models.Model):
     title = models.CharField(max_length=127,)
     url = models.URLField(blank=True,)
     image_url = models.URLField(blank=True,)
-    developer = models.ForeignKey(StoreUser,)    # creator of game
+    developer = models.ForeignKey(StoreUser, related_name = "games", related_query_name = "game")    # creator of game, games can now be queried per storeuser. (https://docs.djangoproject.com/en/dev/ref/models/fields/#django.db.models.ForeignKey.related_name)
     created = models.DateTimeField(default=timezone.now,)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0) #max price 999, cents possible
     description = models.CharField(max_length= 255,)
+    raters = models.ManyToManyField(StoreUser,)
+    ratingsum = models.IntegerField(default = 0)
+    rating = models.IntegerField(default = 0)
 
     CATEGORY_CHOISES = [
     ('ACTION', "Action"),
@@ -44,6 +47,17 @@ class Game(models.Model):
 
     category = models.CharField(max_length = 12, choices = CATEGORY_CHOISES)
 
+#Function to rate the game. Return True if rating succeeded, false if the user has already rated.
+    def rate(self,rating,obj):
+        if rating > 0 and rating <= 5:
+            if self.raters.filter(pk = obj.pk).exists():
+                return False
+            else:
+                self.raters.add(obj)
+                self.ratingsum = self.ratingsum + rating
+                self.rating = self.ratingsum/self.raters.count()
+                self.save()
+                return True
 
 class UserInventory():
     user = models.OneToOneField(StoreUser, on_delete=models.CASCADE, primary_key=True,)
